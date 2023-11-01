72db286b1aeb2135d53b3475953b8af82df5dcea
tonted <tblanco@student.42quebec.com>
2023-11-01 13:55:53 -0400

    Database and wordpress are links!

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Définir les variables d'environnement
-# export DB_NAME="TONTED"
-# export DB_USER="TONTED"
-# export DB_PASSWORD="TONTED"
-# export ROOT_PASSWORD="root"
-
 # Démarrer le service MariaDB
 service mysql start
 
@@ -13,16 +7,16 @@ service mysql start
 sleep 5
 
 # Créer la base de données
-mysql -u root -p${ROOT_PASSWORD} -e "CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;"
+mysql -u root -p -e "CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;"
 
 # Créer l'utilisateur et attribuer des privilèges
-mysql -u root -p${ROOT_PASSWORD} -e "CREATE USER IF NOT EXISTS \`${DB_USER}\`@'localhost' IDENTIFIED BY '${DB_PASSWORD}';"
-mysql -u root -p${ROOT_PASSWORD} -e "GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO \`${DB_USER}\`@'localhost';"
+mysql -u root -p -e "CREATE USER IF NOT EXISTS \`${DB_USER}\`@'wordpress.srcs_inception' IDENTIFIED BY '${DB_PASSWORD}';"
+mysql -u root -p -e "GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO \`${DB_USER}\`@'wordpress.srcs_inception';"
 
 # Rafraîchir les privilèges
-mysql -u root -p${ROOT_PASSWORD} -e "FLUSH PRIVILEGES"
+mysql -u root -p -e "FLUSH PRIVILEGES"
 
 # Arrêter le service MariaDB
-mysqladmin -u root -p${ROOT_PASSWORD} shutdown
+mysqladmin -u root -p shutdown
 
 exec mysqld_safe
