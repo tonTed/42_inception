40dc963ca676487fe184f13c00d2b52573825796
tonted <tblanco@student.42quebec.com>
2023-11-10 16:22:12 -0500

    Exposition sur port 80 marche Script pour wordpress et mariadb amélioré Dockerfile wordpress effectif

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,22 +1,58 @@
 #!/bin/bash
 
+# Fonction pour afficher un message en vert
+log_success() {
+  echo -e "\033[32m$1\033[0m"  # Vert
+}
+
+# Fonction pour afficher un message en jaune
+log_wait() {
+  echo -e "\033[33m$1\033[0m"  # Jaune
+}
+
+# Fonction pour afficher un message en bleu
+log_info() {
+  echo -e "\033[34m$1\033[0m"  # Bleu
+}
+
 # Démarrer le service MariaDB
+log_info "Démarrage du service MariaDB..."
 service mysql start
 
 # Attendre que le service MariaDB démarre
-sleep 5
+sleep 1
+log_info "Attente du démarrage du service MariaDB..."
+while true; do
+    if mysqladmin ping -h localhost -u root -p  --silent; then
+        break
+    fi
+    log_info "En attente du démarrage du service MariaDB..."
+    sleep 1
+done
+log_success "Le service MariaDB est démarré avec succès."
 
 # Créer la base de données
-mysql -u root -p -e "CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;"
+sleep 1
+log_info "Création de la base de données..."
+mysql -u root -p  -e "CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;"
+log_success "Base de données créée avec succès."
 
 # Créer l'utilisateur et attribuer des privilèges
-mysql -u root -p -e "CREATE USER IF NOT EXISTS \`${DB_USER}\`@'wordpress.srcs_inception' IDENTIFIED BY '${DB_PASSWORD}';"
-mysql -u root -p -e "GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO \`${DB_USER}\`@'wordpress.srcs_inception';"
+sleep 1
+log_info "Création de l'utilisateur et attribution des privilèges..."
+mysql -u root -p  -e "CREATE USER IF NOT EXISTS \`${DB_USER}\`@'wordpress.srcs_inception' IDENTIFIED BY '${DB_PASSWORD}';"
+mysql -u root -p  -e "GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO \`${DB_USER}\`@'wordpress.srcs_inception';"
+log_success "Utilisateur créé et privilèges attribués avec succès."
 
 # Rafraîchir les privilèges
-mysql -u root -p -e "FLUSH PRIVILEGES"
+sleep 1
+log_info "Rafraîchissement des privilèges..."
+mysql -u root -p  -e "FLUSH PRIVILEGES"
+log_success "Privilèges rafraîchis avec succès."
 
 # Arrêter le service MariaDB
-mysqladmin -u root -p shutdown
+sleep 1
+log_info "Arrêt du service MariaDB..."
+mysqladmin -u root -p  shutdown
 
 exec mysqld_safe
---
+++ srcs/requirements/wordpress/tools/script.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+
+# Fonction pour afficher un message en vert
+log_success() {
+  echo -e "\033[32m[ENTRY SCRIPT] $1\033[0m"  # Vert
+}
+
+# Fonction pour afficher un message en jaune
+log_wait() {
+  echo -e "\033[33m[ENTRY SCRIPT] $1\033[0m"  # Jaune
+}
+
+# Fonction pour afficher un message en bleu
+log_info() {
+  echo -e "\033[34m[ENTRY SCRIPT] $1\033[0m"  # Bleu
+}
+
+# Attendre que MariaDB soit en cours d'exécution
+sleep 1
+log_info "Attente du démarrage de MariaDB..."
+while ! mysqladmin ping -h"$DB_HOST" -u"$DB_USER" -p"$DB_PASSWORD" --silent; do
+  log_info "En attente du démarrage de MariaDB..."
+  sleep 1
+done
+log_success "MariaDB est en cours d'exécution."
+
+# Attendre que la base de données soit créée
+sleep 1
+log_info "Attente de la création de la base de données WordPress..."
+while ! mysql -h"$DB_HOST" -u"$DB_USER" -p"$DB_PASSWORD" -e "USE $DB_NAME" 2>/dev/null; do
+  log_info "En attente de la création de la base de données WordPress..."
+  sleep 1
+done
+log_success "Base de données WordPress créée avec succès."
+
+# Créer la configuration WordPress
+sleep 1
+log_success "Création de la configuration WordPress..."
+wp config create --dbname="$DB_NAME" --dbuser="$DB_USER" --dbpass="$DB_PASSWORD" --dbhost="$DB_HOST" --locale="ca_FR" --path="/var/www/wordpress"
+log_success "Configuration WordPress créée avec succès."
+
+# Installer WordPress
+sleep 1
+log_success "Installation de WordPress..."
+wp core install --url="$WP_DOMAIN" --title="$DB_NAME" --admin_user="$WP_ADMIN" --admin_password="$WP_ADMIN_PASSWORD" --admin_email="$WP_EMAIL" --skip-email --path="/var/www/wordpress"
+log_success "WordPress installé avec succès."
+
+# Créer un utilisateur WordPress
+sleep 1
+log_success "Création de l'utilisateur WordPress..."
+wp user create "$WP_USER" "$WP_USER_EMAIL" --role=author --user_pass="$WP_USER_PASSWORD" --path="/var/www/wordpress"
+log_success "Utilisateur WordPress créé avec succès."
+
+# Démarrer PHP-FPM
+sleep 1
+log_info "Démarrage de PHP-FPM..."
+php-fpm81 --nodaemonize
+log_success "PHP-FPM est démarré avec succès."
